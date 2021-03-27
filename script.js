193f676818cabfc5375f005aef88f90797704c92
Prasanna Dangol <1905915@kiit.ac.com>
2021-03-27 12:21:32 +0545

    ver 0.2

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const api= {
-    key: "e71f1080f788f47b021779cad8157ad6",
-    baseurl: "https://api.openweathermap.org/data/2.5"
+var d = new Date();
+d.getDay();
+document.getElementById("date").innerHTML = d;
+
+const navSlide = () => {
+    const burger = document.querySelector('.navbox');
+    const nav = document.querySelector('.')
 }
-
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress',setQuery);
-
-function setQuery(evt){
-    if (evt.keyCode == 13) {
-        //getResults(searchbox.value);
-
-        console.log(searchbox.value);
-    }
-}
-
-
